412e45054238ed843c087815b6ac471791017cfe
Siddharth Kumar <144512053+Sid541@users.noreply.github.com>
2025-12-04 15:14:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
-}
+let arr = [1, 2, 3, 4, 5];
+ function minCost(arr){
+     let mincost = 0;
+     while(arr.length>1){
+         arr.sort((a, b) => a - b);
+         let sum = arr[0]+arr[1];
+         mincost+=sum;
+         arr.splice(0,2,sum);
+ }
+ return mincost;
+ }
+ console.log(minCost(arr));
 
 module.exports=mincost;
